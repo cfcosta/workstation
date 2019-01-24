fa561e08fdbd2d15f698267cd17714412df61bdc
Cainã Costa <cainan.costa@gmail.com>
2019-01-24 00:55:40 -0200

    Add spin name to PROMPT

--- deploy/base/custom.zsh
+++ deploy/base/custom.zsh
@@ -4,7 +4,6 @@
 set promptsubst
 
 # Environment Variables
-export RPROMPT="($SPIN)"
 export EDITOR='nvim'
 export SKIM_DEFAULT_COMMAND='rg --files'
 
@@ -22,3 +21,13 @@ alias gs='git stash'
 alias gsp='git stash pop'
 alias ls='exa -l --git'
 alias vim='nvim'
+
+# PROMPT
+local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+
+PROMPT='%{$fg[cyan]%}$SPIN%{$reset_color%} ${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
