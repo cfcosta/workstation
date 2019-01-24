d6d096c7d241413293f64de754aa4cf3b9aecd19
Cainã Costa <cainan.costa@gmail.com>
2019-01-24 21:16:38 -0200

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+failed_dependency() {
+  local message=$1
+
+  echo "Script failed because of missing dependency: $message."
+  exit 1
+}
+
+if ! which docker &>/dev/null; then
+  failed_dependency "Docker is not installed"
+fi
+
+if ! which git &>/dev/null; then
+  failed_dependency "Git is not installed"
+fi
+
+if ! which docker-compose &>/dev/null; then
+  failed_dependency "Docker Compose is not installed"
+fi
+
+if [[ $(whoami) = "root" ]]; then
+  failed_dependency "Can not be installed as root"
+fi
+
+if ! getent group docker | grep $(whoami) &>/dev/null; then
+  failed_dependency "User is not on the Docker group"
+fi
+
+echo "Cloning repository..."
+
+git clone git://github.com/cfcosta/workstation $HOME/.workstation
+
+echo "Adding binary to \$PATH"
+
+sudo ln -s $HOME/.workstation/bin/workstation /usr/local/bin/workstation
+
